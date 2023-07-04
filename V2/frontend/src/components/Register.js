91442ae62e56e2da61bcf84e34c2627628c08981
LizardoMTitua-aM <lmtituana2@espe.edu.ec>
2023-07-04 18:14:39 -0500

    Registro
    Se añadieron mas campos al registro

--- V2/frontend/src/components/Register.js
+++ V2/frontend/src/components/Register.js
@@ -3,12 +3,15 @@ import './Register.css'; // Importa el archivo de estilos CSS
 
 function Register() {
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [gender, setGender] = useState('');
+  const [typeUser, setTypeUser] = useState('');
+  const [nPhone, setNPhone] = useState('');
   const [password, setPassword] = useState('');
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log('Registro de usuario:', name, email, password);
+    console.log('Registro de usuario:', name, lastname, nPhone, password);
   };
 
   return (
@@ -18,17 +21,41 @@ function Register() {
         <input
           className="register-input"
           type="text"
-          placeholder="Nombre completo"
+          placeholder="Nombres"
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
         />
         <input
+        className="register-input"
+        type="text"
+        placeholder="Apellido"
+        value={lastname}
+        onChange={(e)=> setLastName(e.target.value)}
+        required
+        />
+        <input
           className="register-input"
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          type="genero"
+          placeholder="Genero"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Tipo de usuario"
+          value={typeUser}
+          onChange={(e) => setTypeUser(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Número de telefono"
+          value={nPhone}
+          onChange={(e) => setNPhone(e.target.value)}
           required
         />
         <input
