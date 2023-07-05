33b5e778b53701cc7ca760424af55df1ab681792
RafaelOrtiz8995 <66143200+RafaelOrtiz8995@users.noreply.github.com>
2023-07-05 17:19:33 -0500

    PagRegistro1.2

--- V2/frontend/src/components/Register.js
+++ V2/frontend/src/components/Register.js
@@ -3,15 +3,24 @@ import './Register.css'; // Importa el archivo de estilos CSS
 
 function Register() {
   const [name, setName] = useState('');
-  const [lastname, setLastName] = useState('');
-  const [gender, setGender] = useState('');
-  const [typeUser, setTypeUser] = useState('');
+  const [userType, setUserType] = useState('');
   const [nPhone, setNPhone] = useState('');
   const [password, setPassword] = useState('');
+  const [photo, setPhoto] = useState(null);
+  const [province, setProvince] = useState('');
+  const [city, setCity] = useState('');
+  const [mainStreet, setMainStreet] = useState('');
+  const [secondaryStreet, setSecondaryStreet] = useState('');
+  const [postalCode, setPostalCode] = useState('');
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log('Registro de usuario:', name, lastname, nPhone, password);
+    console.log('Registro de usuario:', name, userType, nPhone, password, photo, province, city, mainStreet, secondaryStreet, postalCode);
+  };
+
+  const handlePhotoUpload = (e) => {
+    const file = e.target.files[0];
+    setPhoto(URL.createObjectURL(file));
   };
 
   return (
@@ -26,34 +35,20 @@ function Register() {
           onChange={(e) => setName(e.target.value)}
           required
         />
-        <input
-        className="register-input"
-        type="text"
-        placeholder="Apellido"
-        value={lastname}
-        onChange={(e)=> setLastName(e.target.value)}
-        required
-        />
-        <input
+        <select
           className="register-input"
-          type="genero"
-          placeholder="Genero"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
+          value={userType}
+          onChange={(e) => setUserType(e.target.value)}
           required
-        />
+        >
+          <option value="">Seleccionar tipo de usuario</option>
+          <option value="Vendedor">Vendedor</option>
+          <option value="Cliente">Cliente</option>
+        </select>
         <input
           className="register-input"
           type="text"
-          placeholder="Tipo de usuario"
-          value={typeUser}
-          onChange={(e) => setTypeUser(e.target.value)}
-          required
-        />
-        <input
-          className="register-input"
-          type="text"
-          placeholder="Número de telefono"
+          placeholder="Número de teléfono"
           value={nPhone}
           onChange={(e) => setNPhone(e.target.value)}
           required
@@ -64,6 +59,53 @@ function Register() {
           placeholder="Contraseña"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="file"
+          accept="image/*"
+          onChange={handlePhotoUpload}
+        />
+        {photo && <img src={photo} alt="Foto de perfil" />}
+        <h3 className="register-subtitle">Dirección</h3>
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Provincia"
+          value={province}
+          onChange={(e) => setProvince(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Ciudad"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Calle Principal"
+          value={mainStreet}
+          onChange={(e) => setMainStreet(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Calle Secundaria"
+          value={secondaryStreet}
+          onChange={(e) => setSecondaryStreet(e.target.value)}
+          required
+        />
+        <input
+          className="register-input"
+          type="text"
+          placeholder="Código Postal"
+          value={postalCode}
           required
         />
         <button className="register-button" type="submit">Registrarse</button>
