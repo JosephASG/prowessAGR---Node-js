908af790ed113d3cacef31863e2e262ffa3da055
RafaelOrtiz8995 <66143200+RafaelOrtiz8995@users.noreply.github.com>
2023-07-10 16:55:13 -0500

    Tienda0.1

--- V2/frontend/src/components/StorePage.js
+++ V2/frontend/src/components/StorePage.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './StorePage.css'; // Importa el archivo de estilos CSS
 
 function StorePage() {
+  const categories = ['Frutas', 'Verduras', 'Cereales', 'Hortalizas'];
+  
   return (
     <div className="store-page">
-      <h1 className="store-page-title">Tienda</h1>
+      <div className="sidebar">
+        <h2>Categorías</h2>
+        <ul>
+          {categories.map(category => (
+            <li key={category}>{category}</li>
+          ))}
+        </ul>
+      </div>
+      
       {/* Contenido adicional de la página de la tienda */}
     </div>
   );
