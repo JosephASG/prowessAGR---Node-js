4cee23bb706443b11a26f1b62aa5c324e8d3f0b8
DavidGLeal <adguarderas@espe.edu.ec>
2023-07-14 17:20:10 -0500

    Backend index - Guarderas

---
+++ V2/backend/index.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+
+//import express from "express";
+
+//const app = express();
+//const port = 5000;
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqnpF_ppBXsSawkDiVzYzm2oAV1zLvGWQ",
+  authDomain: "prowess-web-database.firebaseapp.com",
+  projectId: "prowess-web-database",
+  storageBucket: "prowess-web-database.appspot.com",
+  messagingSenderId: "519296320778",
+  appId: "1:519296320778:web:739cc55990bd1a6e4866f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+/*app.listen(port,() =>{
+    console.log("Servidor en operación (Puerto 5000).")
+})*/
