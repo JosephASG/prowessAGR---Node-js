768b8610a26351d83d20e7e896ac531159ec0ffb
RafaelOrtiz8995 <66143200+RafaelOrtiz8995@users.noreply.github.com>
2023-07-18 16:17:38 -0500

    Vendedores1.1

--- V2/frontend/src/components/VendorsPage.js
+++ V2/frontend/src/components/VendorsPage.js
@@ -1,49 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VendorsPage.css'; // Importa el archivo de estilos CSS
 
 function VendorsPage() {
+  const [vendors, setVendors] = useState([
+    {
+      id: 1,
+      name: 'Juan',
+      city: 'Quito',
+      address: 'Dirección 1',
+      phoneNumber: '123456789',
+      whatsappNumber: '987654321',
+      image: 'https://example.com/vendor1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Pedro',
+      city: 'Ambato',
+      address: 'Dirección 2',
+      phoneNumber: '123456789',
+      whatsappNumber: '987654321',
+      image: 'https://example.com/vendor2.jpg',
+    },
+    {
+      id: 3,
+      name: 'Carlos',
+      city: 'Quito',
+      address: 'Dirección 1',
+      phoneNumber: '123456789',
+      whatsappNumber: '987654321',
+      image: 'https://example.com/vendor1.jpg',
+    },
+    {
+      id: 4,
+      name: 'Ricardo',
+      city: 'Ambato',
+      address: 'Dirección 2',
+      phoneNumber: '123456789',
+      whatsappNumber: '987654321',
+      image: 'https://example.com/vendor2.jpg',
+    },
+    {
+      id: 5,
+      name: 'Juan Carlos',
+      city: 'Ambato',
+      address: 'Dirección 2',
+      phoneNumber: '123456789',
+      whatsappNumber: '987654321',
+      image: 'https://example.com/vendor2.jpg',
+    },
+  
+    // Agrega más vendedores según sea necesario
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const vendorsPerPage = 8;
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastVendor = currentPage * vendorsPerPage;
+  const indexOfFirstVendor = indexOfLastVendor - vendorsPerPage;
+  const currentVendors = vendors.slice(indexOfFirstVendor, indexOfLastVendor);
+
+  const totalPages = Math.ceil(vendors.length / vendorsPerPage);
+
   return (
     <div className="vendors-page">
-      <h1 className="vendors-page-title">Vendedores</h1>
-      <div className='vendors-cards'>
-        <div className='vendor-card'>
-          <img src='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f632.png' alt='foto'></img>
-            <h2 className='vendor-data'>Venderdor1 Nombre Apellido</h2>
-            <h2 className='vendor-data'>Contactos: 0999999999</h2>
-            <h2 className='vendor-data'>Provincia: Pichincha</h2>
-            <h2 className='vendor-data'>Ciudad: Quito</h2>
-        </div>
-        <div className='vendor-card'>
-          <img src='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f632.png' alt='foto'></img>
-            <h2 className='vendor-data'>Venderdor1 Nombre Apellido</h2>
-            <h2 className='vendor-data'>Contactos: 0999999999</h2>
-            <h2 className='vendor-data'>Provincia: Pichincha</h2>
-            <h2 className='vendor-data'>Ciudad: Quito</h2>
-        </div>
-        <div className='vendor-card'>
-          <img src='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f632.png' alt='foto'></img>
-            <h2 className='vendor-data'>Venderdor1 Nombre Apellido</h2>
-            <h2 className='vendor-data'>Contactos: 0999999999</h2>
-            <h2 className='vendor-data'>Provincia: Pichincha</h2>
-            <h2 className='vendor-data'>Ciudad: Quito</h2>
-        </div>
-        <div className='vendor-card'>
-          <img src='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f632.png' alt='foto'></img>
-            <h2 className='vendor-data'>Venderdor1 Nombre Apellido</h2>
-            <h2 className='vendor-data'>Contactos: 0999999999</h2>
-            <h2 className='vendor-data'>Provincia: Pichincha</h2>
-            <h2 className='vendor-data'>Ciudad: Quito</h2>
-        </div>
-        <div className='vendor-card'>
-          <img src='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f632.png' alt='foto'></img>
-            <h2 className='vendor-data'>Venderdor1 Nombre Apellido</h2>
-            <h2 className='vendor-data'>Contactos: 0999999999</h2>
-            <h2 className='vendor-data'>Provincia: Pichincha</h2>
-            <h2 className='vendor-data'>Ciudad: Quito</h2>
-        </div>
+      <h2>Listado de Vendedores</h2>
+
+      <div className="search-bar">
+        <input type="text" placeholder="Buscar vendedores" />
+        <select>
+          <option value="">Ordenar por</option>
+          <option value="name">Nombre</option>
+          <option value="city">Ciudad</option>
+        </select>
       </div>
 
-      {/* Contenido adicional de la página de vendedores */}
+      <div className="vendor-list">
+        {currentVendors.map((vendor) => (
+          <div key={vendor.id} className="vendor-card">
+            <img src={vendor.image} alt={vendor.name} className="vendor-image" />
+            <h3>{vendor.name}</h3>
+            <p>Ciudad: {vendor.city}</p>
+            <p>Dirección: {vendor.address}</p>
+            <p>Teléfono: {vendor.phoneNumber}</p>
+            <a href={`https://wa.me/${vendor.whatsappNumber}`}>
+              <button className="whatsapp-button">WhatsApp</button>
+            </a>
+          </div>
+        ))}
+      </div>
+
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
+          <span
+            key={pageNumber}
+            className={`page-number ${pageNumber === currentPage ? 'active' : ''}`}
+            onClick={() => handlePageChange(pageNumber)}
+          >
+            {pageNumber}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
