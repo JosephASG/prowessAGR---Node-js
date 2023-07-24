4ff05670252c614518e0a1a3b40e9a3d774c7da4
LizardoMTitua-aM <lmtituana2@espe.edu.ec>
2023-07-24 17:29:43 -0500

    MiCuenta v1.1
    Arreglos

--- V2/frontend/src/components/MyAccountPage.js
+++ V2/frontend/src/components/MyAccountPage.js
@@ -4,9 +4,11 @@ import './MyAccountPage.css'; // Importa el archivo de estilos CSS
 function MyAccountPage() {
   const [vendors, setVendors] = useState([
     {
-      name: 'Melisa',
+      name: 'Melisa Carbajal',
       city: 'Quito',
-      address: 'Dirección 1',
+      provincia: 'Dirección 1',
+      callePrincipal:'calle principal',
+      calleSecundaria: 'calle secundaria',
       phoneNumber: '123456789',
       whatsappNumber: '987654321',
       image: 'https://estaticos-cdn.prensaiberica.es/clip/2aa4b303-331d-4ef6-98d5-55ef6c8da594_source-aspect-ratio_default_0.jpg',
@@ -18,15 +20,18 @@ function MyAccountPage() {
 
   return (
     <div className="my-account-page">
-      <h1 className="my-account-page-title">Mi Cuenta</h1>
       <div className='my-account-container'>
-        <div className='my-account-info'>
+        <div className='my-account-img'>
           <h2>{vendor.name}</h2>
-          <img src={vendor.image} alt={vendor.name} className='info-image'/>
-          <p>Ciudad: {vendor.city}</p>
-          <p>Dirección: {vendor.address}</p>
-          <p>Teléfono: {vendor.phoneNumber}</p>
-          <p>WhatsApp: {vendor.whatsappNumber}</p>
+          <img src={vendor.image} alt={vendor.name} className='info-image' />
+        </div>
+        <div className='my-account-info'>
+          <br/><br/><br/>
+          <p><strong>Provincia: </strong>{vendor.provincia}</p>
+          <p><strong>Ciudad: </strong> {vendor.city}</p>
+          <p><strong>Direccion: </strong> {vendor.callePrincipal} y {vendor.calleSecundaria}</p>
+          <p><strong>Numero de Telefono: </strong>{vendor.phoneNumber}</p>
+          <p><strong>WhathsApp: </strong>{vendor.whatsappNumber}</p>
         </div>
       </div>
     </div>
