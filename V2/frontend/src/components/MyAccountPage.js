d841293765163dd82c151592fadfb18661c25421
LizardoMTitua-aM <lmtituana2@espe.edu.ec>
2023-07-27 19:39:35 -0500

    actualizacionMicauenta

--- V2/frontend/src/components/MyAccountPage.js
+++ V2/frontend/src/components/MyAccountPage.js
@@ -38,6 +38,24 @@ function MyAccountPage() {
       image: 'https://previews.123rf.com/images/yupiramos/yupiramos1605/yupiramos160521144/57463141-dise%C3%B1o-comprador-avatar-ejemplo-gr%C3%A1fico-del-vector-eps10.jpg',
     }
   ];
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: 'Producto 1',
+      price: 10.99,
+      image: 'https://s2.ppllstatics.com/diariovasco/www/multimedia/202106/04/media/cortadas/platano-kUyC-RCIEbjdcaFn9Yc7KKpofzYN-1248x770@Diario%20Vasco-DiarioVasco.jpg',
+      description: 'Descripción del producto 1',
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      price: 19.99,
+      image: 'https://th.bing.com/th/id/R.c6ef1c7c177ba0e205add120ea606bf5?rik=v3wGpu4bFxqMtA&riu=http%3a%2f%2fwww.ibereco.com%2fimagen%2fcompleta%2f0%2f0%2fmanzana-roja-unidad_1.jpg&ehk=YYUtD01LCi9sJPys3KZ4sp83JmIlX0wQrzg79GZBO3w%3d&risl=&pid=ImgRaw&r=0',
+      description: 'Descripción del producto 2',
+    },
+
+   // Agrega más productos según sea necesario
+  ]);
 
   // Obtiene el primer objeto (usuario) del arreglo según el tipo
   const user = userType === 'vendor' ? vendors[0] : buyers[0];
@@ -73,8 +91,15 @@ function MyAccountPage() {
         <div className='my-account-text'>
           {userType === 'vendor' && (
             <>
-              <h2>Informacion adicional</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit magnis tristique est taciti congue, elementum curae justo turpis cras primis nisi tincidunt tortor mus vehicula. Suscipit ut eros viverra nulla elementum mollis facilisi natoque hac, eu arcu fringilla imperdiet ante nisi tincidunt class. Parturient nibh egestas curae hendrerit rutrum purus nec tempus vehicula pretium id luctus quam ante, metus posuere suscipit platea facilisis et erat sed viverra sagittis scelerisque enim turpis.</p>
+              <h2>Mis Productos</h2>
+              {products.map(product => (
+                 <div key={product.id} className="product-item">
+                 <img src={product.image} alt={product.name} className="product-image" />
+                 <h3>{product.name}</h3>
+                 <p>{product.description}</p>
+                 <p>Precio: ${product.price}</p>
+                </div>
+              ))}
             </>
           )}
         </div>
