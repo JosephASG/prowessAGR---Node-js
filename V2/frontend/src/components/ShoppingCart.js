22e53e7eb31d68406fa78e6631f306149a4357f2
Josue302 <137956060+Josue302@users.noreply.github.com>
2023-08-02 17:38:08 -0500

    Revert "Merge branch 'main' of https://github.com/prowessagricolaweb/prowessAGR---Node-js"
    This reverts commit 2d5cc6d4137b2e7a0143a9d049bf8cf0f776458c, reversing
    changes made to 24c2c1a0f32f6c4c66528e80ea3d808cfe8ae049.

--- V2/frontend/src/components/ShoppingCart.js
+++ V2/frontend/src/components/ShoppingCart.js
@@ -4,14 +4,12 @@ import './ShoppingCart.css'; // Importa el archivo de estilos CSS
 const initialProducts = [
   {
     nombre: 'Producto 1',
-    marca: 'marca1',
     precio: 10,
     cantidad: 2,
     imagen: 'https://elegifruta.com.ar/wp-content/uploads/2017/07/manzana_roja.jpg',
   },
   {
     nombre: 'Producto 2',
-    marca: 'marca2',
     precio: 20,
     cantidad: 1,
     imagen: 'https://statics-cuidateplus.marca.com/cms/styles/natural/azblob/platanos_0.jpg.webp?itok=Nm5QVrwg',
@@ -31,51 +29,35 @@ function ShoppingCart() {
     product.nombre.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const calculateTotalPrice = () => {
-    const total = filteredProducts.reduce((sum, product) => sum + product.precio * product.cantidad, 0);
-    return total;
-  };
-
   return (
     <div className="shopping-cart">
       <h1>Carrito de Compras</h1>
       Buscar: <input
-        className='search'
+      className='search'
         type="text"
         placeholder="Buscar producto..."
         value={searchQuery}
         onChange={handleSearchChange}
       />
-      <div className='shopping-cart-info'>
-        <div className='show-products'>
-          {filteredProducts.map((product, index) => (
-            <div className='producto-cart' key={index}>
-              <div className='img-product'>
-                <img src={product.imagen} alt={product.nombre} />
-              </div>
-              <div className='name-product'>
-                <h3>{product.nombre}</h3>
-                <p><b>Marca:</b> {product.marca}</p>
-                <p><b>Precio:</b> ${product.precio}</p>
-              </div>
-              <div className='cantidad-product'>
-                <button className='btn-add'>+</button>
-                <span className='product-amount'>{product.cantidad}</span>
-                <button className='btn-remove'>-</button>
-              </div>
-              <div className='price-product'>
-                <p><b>Total:</b> ${product.precio * product.cantidad}</p>
-                <div>
-                  <img className='btn-delete' src='https://img.icons8.com/?size=512&id=102550&format=png' alt='delete' />
-                </div>
-              </div>
+      <div className='show-products'>
+        {filteredProducts.map((product, index) => (
+          <div className='producto-cart' key={index}>
+            <div className='img-product'>
+              <img src={product.imagen} alt={product.nombre} />
             </div>
-          ))}
-          <div className='total-price'>
-            <p><b>Precio Total de la Compra:</b> ${calculateTotalPrice()}</p>
-            <button className='btn-buy'><b>Comprar</b></button>
+            <div className='name-product'>
+              <h3>{product.nombre}</h3>
+            </div>
+            <div className='price-product'>
+              <p>Precio: ${product.precio}</p>
+            </div>
+            <div className='cantidad-product'>
+              <button className='btn-add'>+</button>
+              <span className='product-amount'>{product.cantidad}</span>
+              <button className='btn-remove'>-</button>
+              </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
