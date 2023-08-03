3b958babbe1820267b692632791b6c7ef22c54e6
LizardoMTitua-aM <lmtituana2@espe.edu.ec>
2023-08-03 17:29:09 -0500

    CarritoV1.0
    Nuevo Carrito

--- V2/frontend/src/components/ShoppingCart.js
+++ V2/frontend/src/components/ShoppingCart.js
@@ -19,16 +19,29 @@ const initialProducts = [
   // Agrega más productos si es necesario
 ];
 
+const categoriesObject = {
+  categoria1: "Categoría 1",
+  categoria2: "Categoría 2",
+  categoria3: "Categoría 3",
+  // Agrega más categorías según sea necesario
+};
+
 function ShoppingCart() {
   const [products, setProducts] = useState(initialProducts);
   const [searchQuery, setSearchQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
   const filteredProducts = products.filter((product) =>
-    product.nombre.toLowerCase().includes(searchQuery.toLowerCase())
+    product.nombre.toLowerCase().includes(searchQuery.toLowerCase()) &&
+    (selectedCategory === '' || selectedCategory === product.categoria)
   );
 
   const calculateTotalPrice = () => {
@@ -38,14 +51,26 @@ function ShoppingCart() {
 
   return (
     <div className="shopping-cart">
-      <h1>Carrito de Compras</h1>
-      Buscar: <input
-        className='search'
-        type="text"
-        placeholder="Buscar producto..."
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
+      <div className='presentation'>
+        <div className='tittle-page'>
+          <h1>Carrito de compras</h1>
+        </div>
+        <div className='filters'>
+          BUSCAR:<input
+            className='search'
+            type="text"
+            placeholder="Buscar producto..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          CATEGORIAS: <select value={selectedCategory} onChange={handleCategoryChange}>
+            <option value=''>--Seleccionar por Categoria--</option>
+            {Object.entries(categoriesObject).map(([key, value]) => (
+              <option key={key} value={key}>{value}</option>
+            ))}
+          </select>
+        </div>
+      </div>
       <div className='shopping-cart-info'>
         <div className='show-products'>
           {filteredProducts.map((product, index) => (
@@ -55,7 +80,7 @@ function ShoppingCart() {
               </div>
               <div className='name-product'>
                 <h3>{product.nombre}</h3>
-                <p><b>Marca:</b> {product.marca}</p>
+                <p><b>Categoria:</b> {product.marca}</p>
                 <p><b>Precio:</b> ${product.precio}</p>
               </div>
               <div className='cantidad-product'>
