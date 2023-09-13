3c528071428dc11ffddcb4cdd790116af59a4e00
Cristopher Zambrano <93298461+CristopherEspe@users.noreply.github.com>
2023-09-13 16:59:24 -0500

    fucking cart

--- V2/frontend/src/index.js
+++ V2/frontend/src/index.js
@@ -1,41 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar';
-import Footer from './components/Footer'; // Importa el componente Footer
-import HomePage from './components/HomePage';
-import StorePage from './components/StorePage';
-import VendorsPage from './components/VendorsPage';
-import AboutUs from './components/AboutUs';
-import MyAccountPage from './components/MyAccountPage';
-import ShoppingCart from './components/ShoppingCart';
-import RegisterPage from './components/Register'; // Importa el componente RegisterPage (registro.js)
-import ProductList from './components/ProductList';
-import TermsConditions from './components/TermsConditions';
-import SaleDetailsPage from './components/SaleDetailsPage';
-import SalesPage from './components/SalesPage';
-import Login from './components/Login';
+import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <NavigationBar />
-
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/tienda" element={<StorePage />} />
-      <Route path="/vendedores" element={<VendorsPage />} />
-      <Route path="/nosotros" element={<AboutUs />} />
-      <Route path="/mi-cuenta" element={<MyAccountPage />} />
-      <Route path="/carrito" element={<ShoppingCart />} />
-      <Route path="/registro" element={<RegisterPage />} /> {/* Agrega la ruta para la página de registro */}
-      <Route path="productList" element={<ProductList />} />
-      <Route path="/terms&conditions" element={<TermsConditions />} />
-      <Route path="/sale:id" element={<SaleDetailsPage />} />
-      <Route path="/sales" element={<SalesPage />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-
-    <Footer /> {/* Agrega el componente Footer */}
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
