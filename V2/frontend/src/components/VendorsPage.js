f374b2bf19d94f9151273305f409421f83edbe89
adrianC05 <aacarrion4@espe.edu.ec>
2023-09-19 12:48:25 -0500

    mostrar vendedores

--- V2/frontend/src/components/VendorsPage.js
+++ V2/frontend/src/components/VendorsPage.js
@@ -3,31 +3,44 @@ import './VendorsPage.css';
 import SearchBar from './SearchBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
-import ModalEditVendors from './ModalEditVendors'; // Importa el componente ModalEditVendors
-import ModalAddVendor from './ModalAddVendor'; // Importa el componente ModalAddVendor
+import ModalEditVendors from './ModalEditVendors';
+import ModalAddVendor from './ModalAddVendor';
 
 function VendorsPage() {
-  const [vendors, setVendors] = useState([
-    // Tu lista de vendedores existente...
-  ]);
+  const [vendors, setVendors] = useState([]);
+  const [showAddModal, setShowAddModal] = useState(false); // Define setShowAddModal
+  const [editingVendorId, setEditingVendorId] = useState(null); // Define editingVendorId
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const vendorsPerPage = 8;
-
-  const [searchTerm, setSearchTerm] = useState('');
   const [sortOption, setSortOption] = useState('');
-  
-  const [showEditModal, setShowEditModal] = useState(false); // Estado para mostrar/ocultar el modal de edición
-  const [editingVendorId, setEditingVendorId] = useState(null); // ID del vendedor que se está editando
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filterVendor, setFilterVendor] = useState('');
+  const [sortCriteria, setSortCriteria] = useState(null);
 
-  const [showAddModal, setShowAddModal] = useState(false); // Estado para mostrar/ocultar el modal de agregar
+  const vendorsPerPage = 10;
 
-  const handleSearch = (event) => {
-    const { value } = event.target;
-    if (value) {
-      setSearchTerm(value.toLowerCase());
-    } else {
-      setSearchTerm('');
+  const startIndex = (currentPage - 1) * vendorsPerPage;
+  const endIndex = startIndex + vendorsPerPage;
+  const currentVendors = vendors.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(vendors.length / vendorsPerPage);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/fb/vendedor/getSeller`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error en la solicitud al servidor');
+        }
+        return response.json();
+      })
+      .then((data) => setVendors(data))
+      .catch((error) => console.error('Error al cargar los vendedores', error));
+  }, []);
+
+  const showVendorDetails = (vendorId) => {
+    const vendor = vendors.find((p) => p.id === vendorId);
+    if (vendor) {
+      console.log(`Detalles del vendedor ${vendorId}:`, vendor);
     }
   };
 
@@ -35,59 +48,44 @@ function VendorsPage() {
     setSortOption(event.target.value);
   };
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  const handleAddVendor = (newVendor) => {
-    // Agrega el nuevo vendedor a la lista de vendedores en el estado
-    setVendors([...vendors, newVendor]);
   
-    // Cierra el modal de agregar
-    setShowAddModal(false);
+  const handleEdit = (vendorId) => { // Define handleEdit
+    // Implementa la lógica de edición aquí
   };
 
-  const handleEdit = (vendorId) => {
-    // Abre el modal de edición al hacer clic en el botón de editar
-    setShowEditModal(true);
-    setEditingVendorId(vendorId);
+  const handleDelete = (vendorId) => { // Define handleDelete
+    // Implementa la lógica de eliminación aquí
   };
 
-  const handleDelete = (vendorId) => {
-    // Filtra la lista de vendedores para excluir el vendedor con el ID especificado.
-    const updatedVendors = vendors.filter((vendor) => vendor.id !== vendorId);
+  if (sortCriteria) {
+    vendors.sort((a, b) => (a[sortCriteria] > b[sortCriteria] ? 1 : -1));
+  }
 
-    // Actualiza el estado de la lista de vendedores con la nueva lista sin el vendedor eliminado.
-    setVendors(updatedVendors);
-  };
-
-  const filteredVendors = vendors.filter((vendor) =>
-  vendor.name && searchTerm && vendor.name.toLowerCase().includes(searchTerm.toLowerCase())
-);
-
-  const sortedVendors = [...filteredVendors].sort((a, b) => {
-    if (sortOption === 'name') {
-      return a.name.localeCompare(b.name);
-    } else if (sortOption === 'city') {
-      return a.city.localeCompare(b.city);
-    } else {
-      return 0;
-    }
-  });
-
-  const indexOfLastVendor = currentPage * vendorsPerPage;
-  const indexOfFirstVendor = indexOfLastVendor - vendorsPerPage;
-  const currentVendors = sortedVendors.slice(indexOfFirstVendor, indexOfLastVendor);
-
-  const totalPages = Math.ceil(sortedVendors.length / vendorsPerPage);
+  if (filterVendor) {
+    vendors = vendors.filter(
+      (vendor) =>
+        vendor.pro_nombre.toLowerCase().includes(filterVendor.toLowerCase())
+    );
+  }
 
   return (
     <div className="vendors-page">
       <h2>Listado de Vendedores</h2>
-      <button className="add-vendor-button" onClick={() => setShowAddModal(true)}>
+      <button
+        className="add-vendor-button"
+        onClick={() => setShowAddModal(true)}
+      >
         Agregar Vendedor
       </button>
-      
+
       <SearchBar
         searchTerm={searchTerm}
         sortOption={sortOption}
@@ -100,66 +98,72 @@ function VendorsPage() {
       <div className="vendor-list">
         {currentVendors.map((vendor) => (
           <div key={vendor.id} className="vendor-card">
-            <img src={vendor.image} alt={vendor.name} className="vendor-image" />
+            <img
+              src={vendor.image}
+              alt={vendor.name}
+              className="vendor-image"
+            />
             <h3>{vendor.name}</h3>
             <p>Ciudad: {vendor.city}</p>
             <p>Dirección: {vendor.address}</p>
             <p>Teléfono: {vendor.phoneNumber}</p>
-            <button className="edit-button" onClick={() => handleEdit(vendor.id)}>
-              <FontAwesomeIcon icon={faEdit} /> {/* Ícono de editar */}
+            <button
+              className="edit-button"
+              onClick={() => handleEdit(vendor.id)}
+            >
+              <FontAwesomeIcon icon={faEdit} />
             </button>
             <a href={`https://wa.me/${vendor.whatsappNumber}`}>
               <button className="whatsapp-button">WhatsApp</button>
             </a>
-            <button className="delete-button" onClick={() => handleDelete(vendor.id)}>
-              <FontAwesomeIcon icon={faTrash} /> {/* Ícono de eliminar */}
+            <button
+              className="delete-button"
+              onClick={() => handleDelete(vendor.id)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
             </button>
           </div>
         ))}
       </div>
 
       <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
-          <span
-            key={pageNumber}
-            className={`page-number ${pageNumber === currentPage ? 'active' : ''}`}
-            onClick={() => handlePageChange(pageNumber)}
-          >
-            {pageNumber}
-          </span>
-        ))}
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (pageNumber) => (
+            <span
+              key={pageNumber}
+              className={`page-number ${
+                pageNumber === currentPage ? 'active' : ''
+              }`}
+              onClick={() => handlePageChange(pageNumber)}
+            >
+              {pageNumber}
+            </span>
+          )
+        )}
       </div>
 
-      {/* Renderiza el modal de edición si showEditModal es true */}
-      {showEditModal && (
-        <ModalEditVendors
-          isOpen={showEditModal}
-          onClose={() => {
-            setShowEditModal(false); // Cierra el modal al hacer clic en "Cerrar"
-            setEditingVendorId(null); // Limpia el ID del vendedor
-          }}
-          vendorToEdit={vendors.find((vendor) => vendor.id === editingVendorId)}
-          handleEdit={(editedVendor) => {
-            // Aquí puedes manejar la lógica de edición del vendedor
-            console.log('Editar vendedor', editedVendor);
-          }}
-        />
-      )}
-
-      {/* Renderiza el modal de agregar si showAddModal es true */}
-      {showAddModal && (
-        <ModalAddVendor
-          isOpen={showAddModal}
-          onClose={() => setShowAddModal(false)} // Cierra el modal al hacer clic en "Cerrar"
-          handleAddVendor={(newVendor) => {
-            // Aquí puedes manejar la lógica de agregación del nuevo vendedor
-            console.log('Agregar vendedor', newVendor);
-
-            // Cierra el modal de agregar
-            setShowAddModal(false);
-          }}
-        />
-      )}
+      {/*
+      <ModalEditVendors
+        isOpen={showEditModal}
+        onClose={() => {
+          setShowEditModal(false);
+          setEditingVendorId(null);
+        }}
+        vendorToEdit={vendors.find((vendor) => vendor.id === editingVendorId)}
+        handleEdit={(editedVendor) => {
+          console.log('Editar vendedor', editedVendor);
+        }}
+      />
+*/}
+      <ModalAddVendor
+        isOpen={showAddModal}
+        onClose={() => setShowAddModal(false)}
+        handleAddVendor={(newVendor) => {
+          console.log('Agregar vendedor', newVendor);
+          setShowAddModal(false);
+        }}
+      />
+      
     </div>
   );
 }
