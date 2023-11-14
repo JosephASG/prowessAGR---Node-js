f63bb6ba26f64e74b8c1c2ceadab36f061d0324a
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-11-14 16:51:14 -0500

    PAW-15

--- frontend/src/components/Mapa.js
+++ frontend/src/components/Mapa.js
@@ -2,36 +2,39 @@ import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-
-import customIcon from "../imagenes/marcador.png"; // Importa tu imagen de marcador personalizada
+import customIcon from "../imagenes/marcador.png";
 
 const customMarkerIcon = new L.Icon({
-  iconUrl: customIcon, // Ruta a tu imagen personalizada
-  iconSize: [32, 32], // Tamaño del icono
-  iconAnchor: [16, 32], // Punto de anclaje del icono
-  popupAnchor: [0, -32], // Punto de anclaje del popup
+  iconUrl: customIcon,
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
 });
 
-
-const Map = () => {
+const Mapa = ({ onLocationSelect }) => {
   const [clickedLocation, setClickedLocation] = useState(null);
+
+  const LocationMarker = () => {
+    useMapEvents({
+      click: (e) => {
+        setClickedLocation(e.latlng);
+        onLocationSelect(e.latlng);
+         // Llama a la función de devolución de llamada para pasar la latitud y longitud al componente padre
+      },
+    });
+
+    return null;
+  };
 
   return (
     <div>
-      <MapContainer
-        center={[0, 0]}
-        zoom={4}
-        style={{ width: "100%", height: "400px", zIndex: 1 }}
-        >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-<LocationMarker setClickedLocation={setClickedLocation} />
+      <MapContainer center={[0, 0]} zoom={4} style={{ width: "100%", height: "400px", zIndex: 1 }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LocationMarker />
         {clickedLocation && (
           <Marker position={clickedLocation} icon={customMarkerIcon}>
             <Popup>
-              Latitud: {clickedLocation.lat.toFixed(4)}, Longitud:{" "}
-              {clickedLocation.lng.toFixed(4)}
+              Latitud: {clickedLocation.lat.toFixed(4)}, Longitud: {clickedLocation.lng.toFixed(4)}
             </Popup>
           </Marker>
         )}
@@ -40,14 +43,4 @@ const Map = () => {
   );
 };
 
-const LocationMarker = ({ setClickedLocation }) => {
-  useMapEvents({
-    click: (e) => {
-setClickedLocation(e.latlng);
-    },
-  });
-
-  return null;
-};
-
-export default Map;
+export default Mapa;
