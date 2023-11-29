e188261d8b8a2ef3a09bc0d762ab1a7cba84aae5
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-11-29 15:49:50 -0500

    creation of modalltypeproduct

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './Footer.css';
-import facebook from '../imagenes/facebook.png';
-import tiktok from '../imagenes/tiktok.png';
-import twitter from '../imagenes/twitter.png';
-import ig from '../imagenes/ig.png'
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        {/* Aquí puedes agregar el contenido que desees para el footer */}
- 
+
         <p>Todos los derechos reservados &copy; {new Date().getFullYear()} Prowess</p>
         <p>Revisa nuestros <a href='terms&conditions'>Términos y Condiciones</a></p>
       </div>
---
+++ frontend/src/components/ModalAddTypeProduct.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import "./ModalAddProducts.css";
+import { getCategories } from "../services/category";
+import { getSellers } from "../services/seller";
+import { postProduct } from "../services/product";
+const WEBURL = process.env.REACT_APP_API_URL
+
+const ModalAddProducts = ({ isOpen, onClose }) => {
+    const [newProduct, setNewProduct] = useState({
+        pro_nombre: "",
+        pro_precio: "",
+        pro_stock: "",
+        pro_descripcion: "",
+        pro_categoria: "",
+        pro_medida: "",
+        pro_vendedor: "",
+        pro_fechaFinal: "",
+        pro_fechaInicio: "",
+        pro_estado: "",
+        pro_imagen: null
+    });
+ 
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (isOpen) {
+            const body = document.body;
+            body.classList.add("modal-open");
+            return () => {
+                body.classList.remove("modal-open");
+            };
+        }
+
+        getCategory(token);
+        getVendedores(token);
+
+    }, [isOpen]);
+
+    const handleInputChange = (e) => {
+        const { name, value, file } = e.target;
+        if (name === "pro_imagen") {
+            const file = e.target.files[0];
+            const formData = new FormData();
+            formData.append("pro_imagen", file);
+            setNewProduct((prevState) => ({
+                ...prevState,
+                pro_imagen: formData
+            }));
+        }
+        else {
+            setNewProduct({
+                ...newProduct,
+                [name]: value
+            });
+        }
+    };
+
+    const handleSave = async () => {
+        const { pro_imagen, ...otrosDatos } = newProduct;
+        const formData = new FormData();
+        if (!pro_imagen) return alert("Debe seleccionar una imagen");
+        var imagen = pro_imagen.get("pro_imagen");
+        formData.append("pro_imagen", imagen);
+        Object.entries(otrosDatos).forEach(([key, value]) => {
+            formData.append(key, value);
+        });
+        const token = localStorage.getItem("token");
+        postProducto(formData, token);
+    };
+
+    const postProducto = async (product, token) => {
+        const response = await postProduct(product, token);
+        if (response.status === 200) {
+            onClose();
+        } else {
+            console.error("Error al agregar el producto en el servidor");
+        }
+
+    }
+
+
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+
+        </div>
+    );
+};
+
+export default ModalAddProducts;
