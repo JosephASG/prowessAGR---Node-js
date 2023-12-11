6f15c320d210d6be792535540f0f81adfdd1f275
JDanielCaler0 <d4nn1_123@hotmail.com>
2023-12-11 12:01:10 -0500

    Contenido Anuncios

--- frontend/src/pages/AdvertisementSection.js
+++ frontend/src/pages/AdvertisementSection.js
@@ -2,16 +2,54 @@
 
 import React from 'react';
 import "./AdvertisementSection.css"; // Puedes crear un archivo de estilo para esta sección si es necesario
+import Anuncio from '../imagenes/Anuncio.png';
 
 function AdvertisementSection() {
   return (
-    <div className="advertisement-section">
-      <h2>Anuncios</h2>
-      {/* Aquí puedes agregar el contenido de tus anuncios */}
-      <p>Anuncio 1: ¡Oferta especial!</p>
-      <p>Anuncio 2: ¡Descuentos exclusivos!</p>
-      {/* ... Puedes agregar más anuncios según sea necesario */}
+    
+      <center>
+      <h1>ANUNCIOS</h1>
+      <div className="advertisement-section">
+      <p>¡DESCUBRE EL PODER DEL EXITO CON PROWESS!</p>
+      <p>OFRECEMOS</p>
+      <p>PLANES MENSUALES</p>
+      <img src={Anuncio} alt="Imagen Anuncio" className="advertisement-image" /><br/>
     </div>
+    <h1>ADVANCE PROWESS</h1>
+    <div className="advertisement-section">
+      <p>- GESTION DE REDES SOCIALES</p>
+      <p>- REDISEÑO DE CATALOGO Y CREACION DE MENU ONLINE (CODIGO QR)</p>
+      <p>- CRONOGRAMA DE CONTENIDO</p>
+      <p>. 4 tiktoks al mes - trafico de tik tok a Instagram (mencion)</p>
+      <p>. 2 pasts + 2 carruseles al mes en Instagram y Facebook</p>
+      <p>- VISUALIZACION EN PLATAFORMAS DE VENTA INSTAGRAM</p>
+      <p>- PUBLICIDAD DE FACEBOOK (CONTRATADA)</p>
+      <p>- INCREMENTO DE SEGUIDORES EN REDES SOCIALES (80 - 100) MENSUALES</p><br/>
+      <h2>PRECIO:</h2>
+      <p>$34,99 + IVA</p>
+    </div>
+    <h1>SILVER PROWESS</h1>
+    <div className="advertisement-section">
+      <p>- GESTION DE REDES SOCIALES</p>
+      <p>- REDISEÑO DE CATALOGOS Y/O MENUS</p>
+      <p>- CRONOGRAMA DE CONTENIDO</p>
+      <p>. 4 post + 1 carrucel al mes para Facebook e Instagram (mencion)</p>
+      <p>. 6 tiktoks al mes</p>
+      <p>- INCREMENTO DE SEGUIDORES 100-120</p>
+      <p>- PUBLICIDAD DE FACEBOOK ( CONTRATADA )</p>
+      <p>- AUMENTO DE VISUALIZACION</p><br/>
+      <h2>PRECIO:</h2>
+      <p>$49,99 + IVA</p>
+    </div>
+    <h1>GOLD PROWESS</h1>
+    <div className="advertisement-section">
+      <h2></h2>
+    </div>
+    <h1>PLATINO PROWESS</h1>
+    <div className="advertisement-section">
+      <h2></h2>
+    </div>
+    </center>
   );
 }
 
