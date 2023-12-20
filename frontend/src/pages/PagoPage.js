0273d70f74d403b9382b3dec2555f23581427f73
JDanielCaler0 <d4nn1_123@hotmail.com>
2023-12-20 11:42:27 -0500

    Pagina de pago

---
+++ frontend/src/pages/PagoPage.js
@@ -0,0 +1,14 @@
+// PagoPage.js
+import React from 'react';
+import './PagoPage.css';
+
+function PagoPage() {
+  return (
+    <div>
+      <h1>Página de Pago</h1>
+      {/* Agrega aquí tu formulario de pago */}
+    </div>
+  );
+}
+
+export default PagoPage;
