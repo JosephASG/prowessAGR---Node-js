69ce9d9675f461786bc9e00571fd43cb12609a08
JDanielCaler0 <d4nn1_123@hotmail.com>
2023-12-22 13:49:39 -0500

    Pago de compra pagina

--- frontend/src/pages/PagoPage.js
+++ frontend/src/pages/PagoPage.js
@@ -5,7 +5,9 @@ import './PagoPage.css';
 function PagoPage() {
   return (
     <div className="pago-page">
-      <h1>Página de Pago</h1>
+      <p>AGREGA UNA TARJETA DE CREDITO O DE DEBITO</p>
+      <p>Todos los campos son obligatorios</p>
+      <p>Se aceptan las siguientes tarjetas</p>
       {/* Agrega aquí tu formulario de pago */}
     </div>
   );
