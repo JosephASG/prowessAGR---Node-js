8958df806e1de1a7eef92c69cf7cff22fbafbb10
Mila <carivera14@espe.edu.ec>
2024-01-22 14:03:18 -0500

    Anuncios funcionales
    Subida de fotos a la nube

--- frontend/src/pages/AdvertisementSection.js
+++ frontend/src/pages/AdvertisementSection.js
@@ -2,10 +2,6 @@
 
 import React from 'react';
 import "./AdvertisementSection.css"; // Puedes crear un archivo de estilo para esta sección si es necesario
-import Planes from '../imagenes/Planes.png';
-import Planes1 from '../imagenes/Planes1.png';
-import Planes2 from '../imagenes/Planes2.png';
-import Planes3 from '../imagenes/Planes3.png';
 
 function AdvertisementSection() {
   return (
@@ -17,10 +13,26 @@ function AdvertisementSection() {
         <p>¡Aumenta la productividad y optimiza tus cultivos con nuestras soluciones avanzadas!</p>
       </div>
       <div className="advertisement-section">
-        <img src={Planes} alt="Imagen Planes" className="advertisement-image" /><br/><br/><br/>
-        <img src={Planes1} alt="Imagen Planes" className="advertisement-image" /><br/><br/><br/>
-        <img src={Planes2} alt="Imagen Planes" className="advertisement-image" /><br/><br/><br/>
-        <img src={Planes3} alt="Imagen Planes" className="advertisement-image" />
+      <img
+        src="https://media.discordapp.net/attachments/819309536414400533/1199063611994284122/Planes.png?ex=65c12e02&is=65aeb902&hm=93ea8d1f80159ef1a3ad0de3d41c1f52bda799b7229186cf088b5856913d4b9b&=&format=webp&quality=lossless&width=1080&height=1080"
+        alt="Imagen Planes"
+        className="advertisement-image"
+      /><br/><br/><br/>
+      <img
+        src="https://media.discordapp.net/attachments/819309536414400533/1199063612283687063/Planes1.png?ex=65c12e02&is=65aeb902&hm=e0c0bb3b3bf5f76a9269842da761dd32be8c5d39d7456273133b75142cfffaf7&=&format=webp&quality=lossless&width=1080&height=1080"
+        alt="Imagen Planes"
+        className="advertisement-image"
+      /><br/><br/><br/>      
+      <img
+        src="https://media.discordapp.net/attachments/819309536414400533/1199063612661178438/Planes2.png?ex=65c12e02&is=65aeb902&hm=1367364a85a26bda3e26d85ebc311d075807804502e3e5e080645b9ea1153c15&=&format=webp&quality=lossless&width=1080&height=1080"
+        alt="Imagen Planes"
+        className="advertisement-image"
+      /><br/><br/><br/>
+      <img
+        src="https://media.discordapp.net/attachments/819309536414400533/1199063611633565716/Planes3.png?ex=65c12e02&is=65aeb902&hm=464bbb3103b71389468287f19c7330df4c232779ebcc18c838f32404f7133c9f&=&format=webp&quality=lossless&width=1080&height=1080"
+        alt="Imagen Planes"
+        className="advertisement-image"
+      /><br/><br/><br/>      
       </div>
     </center>
   );
