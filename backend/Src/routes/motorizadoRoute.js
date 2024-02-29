70acd047cf123715a2d81282486990125a7589dc
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-02-29 14:42:12 -0500

    Update motorizado api

--- backend/Src/routes/motorizadoRoute.js
+++ backend/Src/routes/motorizadoRoute.js
@@ -1,21 +1,21 @@
-// import * as motorizedController from '../controller/motorizedController.js';
-// import express from 'express';
+import * as motorizadoController from '../controller/motorizadoController.js';
+import express from 'express';
 
-// const motorizedRoute = express.Router();
+const motorizadoRoute = express.Router();
 
-// // Create a new motorized controller
-// motorizedRoute.post('/createMotorizedController', motorizedController.createMotorizedController);
+// Create a new motorized controller
+motorizadoRoute.post('/createmotorizadodController', motorizadoController.createmotorizadoController);
 
-// // Get all motorized controllers
-// motorizedRoute.get('/getMotorizedControllers', motorizedController.getMotorizedControllers);
+// Get all motorized controllers
+motorizadoRoute.get('/getmotorizadoControllers', motorizadoController.getmotorizadoControllers);
 
-// // Get a specific motorized controller by ID
-// motorizedRoute.get('/getMotorizedController/:id', motorizedController.getMotorizedControllerByID);
+// Get a specific motorized controller by ID
+motorizadoRoute.get('/getmotorizadoController/:id', motorizadoController.getmotorizadoControllerByID);
 
-// // Update a motorized controller
-// motorizedRoute.put('/updateMotorizedController/:id', motorizedController.updateMotorizedController);
+// Update a motorized controller
+motorizadoRoute.put('/updatemotorizadoController/:id', motorizadoController.updatemotorizadoController);
 
-// // Delete a motorized controller
-// motorizedRoute.delete('/deleteMotorizedController/:id', motorizedController.deleteMotorizedController);
+// Delete a motorized controller
+motorizadoRoute.delete('/deletemotorizadoController/:id', motorizadoController.deletemotorizadoController);
 
-// export default motorizedRoute;
+export default motorizadoRoute;
