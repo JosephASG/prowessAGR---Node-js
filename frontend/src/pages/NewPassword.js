1f5d2ff27f684fa323d9760228eaf11e3f214549
Denep Maliza <demaliza@espe.edu.ec>
2024-03-01 14:58:35 -0500

    Interfaz despues del correo
    only interface

---
+++ frontend/src/pages/NewPassword.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { resetPassword } from '../services/auth'; 
+
+function NewPassword() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState('');
+  const { token } = useParams(); 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError('Las contraseñas no coinciden');
+      return;
+    }
+
+    try {
+      const response = await resetPassword(token, password);
+      if (response.status === 200) {
+        setSuccessMessage('Contraseña restablecida correctamente');
+      } else {
+        setError('Hubo un problema al restablecer la contraseña');
+      }
+    } catch (error) {
+      setError('Hubo un problema al restablecer la contraseña');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Restablecer Contraseña</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="password">Nueva Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+        {successMessage && <div style={{ color: 'green' }}>{successMessage}</div>}
+        <button type="submit">Restablecer Contraseña</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewPassword;
