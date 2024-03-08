f4f0a349b982f03e8d3b76c7e4de7510f576ed53
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-03-08 15:05:29 -0500

    Cmente Passwordsend
    Me salia errores

--- frontend/src/pages/PasswordSend.js
+++ frontend/src/pages/PasswordSend.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './PasswordSend.css';
+// import React from 'react';
+// import './PasswordSend.css';
 
-function PasswordSend() {
-  return (
-    <div className="password-send-container">
-      <h2 className="password-send-title">Correo electrónico de restablecimiento de contraseña enviado</h2>
-      <p className="password-send-message">
-        Se ha enviado un correo electrónico de restablecimiento de contraseña a la dirección de correo electrónico de tu cuenta,
-        pero puede llevar varios minutos que aparezca en tu bandeja de entrada. Por favor, espera al menos 10 minutos antes de intentar otro restablecimiento.
-      </p>
-    </div>
-  );
-}
+// function PasswordSend() {
+//   return (
+//     <div className="password-send-container">
+//       <h2 className="password-send-title">Correo electrónico de restablecimiento de contraseña enviado</h2>
+//       <p className="password-send-message">
+//         Se ha enviado un correo electrónico de restablecimiento de contraseña a la dirección de correo electrónico de tu cuenta,
+//         pero puede llevar varios minutos que aparezca en tu bandeja de entrada. Por favor, espera al menos 10 minutos antes de intentar otro restablecimiento.
+//       </p>
+//     </div>
+//   );
+// }
 
-export default PasswordSend;
+// export default PasswordSend;
