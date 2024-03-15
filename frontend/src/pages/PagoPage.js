359c032bf2d6da0cb22cfba1e6caba6b52f875b8
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-03-15 15:17:45 -0500

    Update pagopage

--- frontend/src/pages/PagoPage.js
+++ frontend/src/pages/PagoPage.js
@@ -34,7 +34,6 @@ function PagoPage({ cart, vendor, clearCart, orden }) {
         nombre: data.data.nombreUsuario,
         apellido: data.data.apellidoUsuario,
         email: data.data.correoUsuario,
-        claveUsuario: data.data.claveUsuario,
         telefono: data.data.telefonoUsuario
       }
       setUsuario(usuario);
