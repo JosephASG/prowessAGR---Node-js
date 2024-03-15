44b61350233b60eee5074ce2bcb1dba4081d02fb
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-03-15 14:54:01 -0500

    Index.js
    Actualziación

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
-import multer from 'multer';
-import routes from "./Src/routes.js";
+import dotenv from "dotenv";
+import multer from "multer";
+import routes from "./src/routes.js";
 
 const app = express();
 app.use(express.json());
 
 // Opciones de configuración para CORS
 const corsOptions = {
-  origin: '*', // Permite solicitudes desde cualquier origen, puedes ajustarlo según tus necesidades
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+  origin: "*", // Permite solicitudes desde cualquier origen, puedes ajustarlo según tus necesidades
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Métodos HTTP permitidos
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
@@ -23,9 +23,25 @@ const port = process.env.PORT || 5000;
 app.setMaxListeners(0);
 
 // Obtener Rutas
-app.use('/', routes);
+app.use("/", routes);
 
+// Ruta principal
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido, PGA!");
+});
+
+app.post("/", (req, res) => {
+  res.send("¡Bienvenido, PGA!");
+});
+
+app.put("/", (req, res) => {
+  res.send("¡Bienvenido, PGA!");
+});
+
+app.delete("/", (req, res) => {
+  res.send("¡Bienvenido, PGA!");
+});
 // Iniciar el servidor y hacer que escuche en el puerto definido
 app.listen(port, () => {
-  console.log(`Servidor en operación (Puerto ${port}).`);
+  console.log(`Servidor iniciado exitosamente`);
 });
