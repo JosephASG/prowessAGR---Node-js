7fcbb415976c7a93f8171f53b376acbbbec1f270
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-03-21 10:26:56 -0500

    PAW-74

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+//Actualizacon en el hostinger 2024/3/21
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
