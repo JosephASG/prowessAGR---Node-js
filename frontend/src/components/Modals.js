63772b9e3e4953c6345b84a10e98ca400950787a
Juan Pablo Pinza Armijos <jppinza@espe.edu.ec>
2024-03-26 10:06:22 -0500

    PAW-64 Implementación de Recuperación de Contraseña
    Implementación de Recuperar Contraseña en localHost

--- frontend/src/components/Modals.js
+++ frontend/src/components/Modals.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ModalPassword from './ModalPassword'
 
 const Modals = ({title}) => {
   return (
     <div>
         <h2>{title}</h2>
+        <ModalPassword>
+          <p>Por favor, ingrese el código enviado a su correo electrónico</p>
+          <input type="text" />
+        </ModalPassword>
     </div>
   )
 }
