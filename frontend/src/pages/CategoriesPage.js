5eaca662756a30c47c30811e1f746e840762ef79
Bryan406 <brquispe@espe.edu.ec>
2024-06-03 14:39:53 -0500

    Actualizacion

--- frontend/src/pages/CategoriesPage.js
+++ frontend/src/pages/CategoriesPage.js
@@ -1,63 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import './CategoriesPage.css';
 import ModalAddCategory from '../components/ModalAddCategory';
 import ModalEditCategory from '../components/ModalEditCategory';
-import { getCategories, deleteCategory, updateCategory} from '../services/category';
+import { getCategories, deleteCategory, updateCategory } from '../services/category';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import Loading from "components/General/Loading";
 
 const CategoryList = () => {
-  const [categories, setCategories] = useState([]); // Cambiamos a "categories" en lugar de "products"
-  const WEBURL = process.env.REACT_APP_API_URL
-  const ITEMS_PER_PAGE = 5;
-
+  const [categories, setCategories] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [categoryToEdit, setCategoryToEdit] = useState(null); // Cambiamos a "categoryToEdit"
+  const [categoryToEdit, setCategoryToEdit] = useState(null);
   const [sortCriteria, setSortCriteria] = useState(null);
   const [filterCategory, setFilterCategory] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-
-  
   const token = localStorage.getItem('token');
+  const [categoryToUpdate, setCategoryToUpdate] = useState(null);
 
-  const [categoryToUpdate, setCategoryToUpdate] = useState(null); // Cambiamos a "categoryToUpdate"
+  const ITEMS_PER_PAGE = 5;
 
   useEffect(() => {
-
     getCategory(token);
-    if(categoriesToDisplay){
-      setIsLoading(true);
-    }
+  }, [token]);
 
-  }, []);
-
-  const getCategory= async (token) => {
+  const getCategory = async (token) => {
     try {
+      setIsLoading(true);
       const res = await getCategories(token);
-      console.log(res);
       const data = res.data;
       setCategories(data);
       setIsLoading(false);
-
     } catch (error) {
       console.error('Error al cargar las categorías', error);
+      setIsLoading(false);
     }
   };
 
   const handleDelete = async (categoryId) => {
-    const token = localStorage.getItem('token');
     const response = await deleteCategory(categoryId, token);
-    if (response.status===200) {
-      setCategories(categories.filter((category) => category.id !== categoryId)); // Cambiamos "products" a "categories"
+    if (response.status === 200) {
+      setCategories(categories.filter((category) => category.id !== categoryId));
     } else {
       console.error('Error al eliminar la categoría en el servidor');
     }
-  }
-
+  };
 
   const handleOpenModal = () => {
     setIsAddModalOpen(true);
@@ -72,30 +62,28 @@ const CategoryList = () => {
     setCategoryToUpdate(editedCategory);
   };
 
+  const handleUpdateCategory = useCallback(async () => {
+    if (!categoryToUpdate) {
+      return;
+    }
+    const response = await updateCategory(categoryToUpdate, token);
+    if (response && response.status === 200) {
+      setCategories((prevCategories) =>
+        prevCategories.map((category) =>
+          category.id === categoryToUpdate.id ? categoryToUpdate : category
+        )
+      );
+      setIsEditModalOpen(false);
+    } else {
+      console.error('Error al guardar los cambios en el servidor');
+    }
+  }, [categoryToUpdate, token]);
+
   useEffect(() => {
     if (categoryToUpdate) {
       handleUpdateCategory();
     }
-  }, [categoryToUpdate]);
-
-  const handleUpdateCategory = async() => {
-    if (!categoryToUpdate) {
-      return;
-    }
-    const token = localStorage.getItem('token');
-    const response = await updateCategory(categoryToUpdate, token);
-    if (response && response.status === 200) {
-      setCategories((prevCategories) =>
-            prevCategories.map((category) =>
-              category.id === categoryToUpdate.id ? categoryToUpdate : category
-            )
-          );
-      setIsEditModalOpen(false);
-    }
-    else {
-      console.error('Error al guardar los cambios en el servidor');
-    }
-  };
+  }, [categoryToUpdate, handleUpdateCategory]);
 
   const handleSort = (criteria) => {
     setSortCriteria(criteria);
@@ -103,6 +91,10 @@ const CategoryList = () => {
 
   const handleFilter = (category) => {
     setFilterCategory(category);
+  };
+
+  const handleSelectCategory = (categoryId) => {
+    setSelectedCategoryId(categoryId);
   };
 
   let sortedAndFilteredCategories = [...categories];
@@ -126,87 +118,90 @@ const CategoryList = () => {
   const categoriesToDisplay = sortedAndFilteredCategories.slice(startIndex, endIndex);
 
   return (
-
-      <Container fluid>
-        {isLoading && <Loading></Loading>}
-        <h1 style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: "1.5px white", color: "white" }}>LISTA DE CATEGORÍAS</h1>
-        <div className='btn-add-container'>
-          <Button onClick={handleOpenModal} className='btn-add-product' style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Agregar Categoría</Button>
-        </div>
-        <Row>
-          <ModalAddCategory isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)} />
-          <ModalEditCategory
-            isOpen={isEditModalOpen}
-            onClose={() => setIsEditModalOpen(false)}
-            categoryToEdit={categoryToEdit}
-            handleEdit={handleEdit}
-          />
-          <Col>
-            <div className="filter-row">
-              <Form.Label style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Filtrar por Categoría:</Form.Label>
-              <Form.Control
-                type="text"
-                value={filterCategory}
-                onChange={(e) => handleFilter(e.target.value)}
-              />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="header-row-category-list">
-              <b onClick={() => handleSort('nombre')} style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Nombre</b>
-              <b style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Descripción</b>
-              <b style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Acciones</b>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="container-products" style={{color: 'black', margin: '10px 50px' }}>
-              {categoriesToDisplay.map((category) => (
-                <div className="category" style={{backgroundColor:'white'}} key={category.id}>
-                  <div>{category.nombreCategoria}</div>
-                  <div>{category.descripcionCategoria}</div>
-                  <div className='actions-container'>
-                    <FontAwesomeIcon
-                      className="fa-icon-edit"
-                      icon={faPenToSquare}
-                      onClick={() => handleEditCategory(category)}
-                    />
-                    <FontAwesomeIcon
-                      className="fa-icon-trash"
-                      icon={faTrash}
-                      onClick={() => handleDelete(category.id)}
-                    />
-                  </div>
+    <Container fluid>
+      {isLoading && <Loading />}
+      <h1 style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: "1.5px white", color: "white" }}>LISTA DE CATEGORÍAS</h1>
+      <div className='btn-add-container'>
+        <Button onClick={handleOpenModal} className='btn-add-product' style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Agregar Categoría</Button>
+      </div>
+      <Row>
+        <ModalAddCategory isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)} />
+        <ModalEditCategory
+          isOpen={isEditModalOpen}
+          onClose={() => setIsEditModalOpen(false)}
+          categoryToEdit={categoryToEdit}
+          handleEdit={handleEdit}
+        />
+        <Col>
+          <div className="filter-row">
+            <Form.Label style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Filtrar por Categoría:</Form.Label>
+            <Form.Control
+              type="text"
+              value={filterCategory}
+              onChange={(e) => handleFilter(e.target.value)}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="header-row-category-list">
+            <b onClick={() => handleSort('nombre')} style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Nombre</b>
+            <b style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Descripción</b>
+            <b style={{ textShadow: "none", fontFamily: "Roboto", WebkitTextStroke: ".1px white", color: "white" }}>Acciones</b>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="container-products" style={{ color: 'black', margin: '10px 50px' }}>
+            {categoriesToDisplay.map((category) => (
+              <div
+                className={`category ${selectedCategoryId === category.id ? 'selected' : ''}`}
+                key={category.id}
+                onClick={() => handleSelectCategory(category.id)}
+                style={{ backgroundColor: selectedCategoryId === category.id ? 'blue' : 'white' }}
+              >
+                <div>{category.nombreCategoria}</div>
+                <div>{category.descripcionCategoria}</div>
+                <div className='actions-container'>
+                  <FontAwesomeIcon
+                    className="fa-icon-edit"
+                    icon={faPenToSquare}
+                    onClick={() => handleEditCategory(category)}
+                  />
+                  <FontAwesomeIcon
+                    className="fa-icon-trash"
+                    icon={faTrash}
+                    onClick={() => handleDelete(category.id)}
+                  />
                 </div>
-              ))}
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="pagination">
-              <Button
-                onClick={() => setCurrentPage(currentPage - 1)}
-                disabled={currentPage === 1}
-              >
-                Anterior
-              </Button>
-              <span style={{color:"white"}}>{currentPage}</span>
-              <Button
-                onClick={() => setCurrentPage(currentPage + 1)}
-                disabled={currentPage === totalPages}
-              >
-                Siguiente
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    
-  );  
+              </div>
+            ))}
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="pagination">
+            <Button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Anterior
+            </Button>
+            <span style={{ color: "white" }}>{currentPage}</span>
+            <Button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Siguiente
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default CategoryList;
--- frontend/src/services/category.js
+++ frontend/src/services/category.js
@@ -1,63 +1,68 @@
-import axios from "axios"
-const WEBURL = process.env.REACT_APP_API_URL
+import axios from "axios";
+const WEBURL = process.env.REACT_APP_API_URL;
 
 export const getCategories = async (token) => {
-    try {
-      const response = await axios.get(`${WEBURL}fb/categoria/get`, {
-        headers: {
-          token: token,
-        },
-      });
-      return response
-    } catch (error) {
-      console.error('Error al cargar las categorías', error);
-    }
-  }
-
-
-
-export const postCategory = async(category, token) => {
- try{
-    const response = await axios.post(`${WEBURL}fb/categoria/post`, JSON.stringify(category) , {
-        headers: {
-            token: token,
-            'Content-Type':'application/json',
-        },
+  try {
+    const response = await axios.get(`${WEBURL}fb/categoria/get`, {
+      headers: {
+        token: token,
+      },
     });
     return response;
- }
- catch (error) {
-    console.error('Error al crear la categoría en el servidor');
- } 
+  } catch (error) {
+    console.error('Error al cargar las categorías', error);
+    throw error;
+  }
+};
 
-}
+export const postCategory = async (category, token) => {
+  try {
+    const response = await axios.post(
+      `${WEBURL}fb/categoria/post`,
+      JSON.stringify(category),
+      {
+        headers: {
+          token: token,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error('Error al crear la categoría en el servidor', error);
+    throw error;
+  }
+};
 
 export const deleteCategory = async (categoryId, token) => {
-    try{
-        const response = await axios.delete(`${WEBURL}fb/categoria/delete/${categoryId}`, {
-            headers: {
-                token: token,
-            },
-        });
-        return response
-    } 
-    catch (error) {
-        console.error('Error al eliminar la categoría en el servidor');
-    }
-}
+  try {
+    const response = await axios.delete(`${WEBURL}fb/categoria/delete/${categoryId}`, {
+      headers: {
+        token: token,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error('Error al eliminar la categoría en el servidor', error);
+    throw error;
+  }
+};
 
 export const updateCategory = async (category, token) => {
-    try{
-        const response = await axios.put(`${WEBURL}fb/categoria/update/${category.id}`, JSON.stringify(category), {
-            headers: {
-                token: token,
-                'Content-Type': 'application/json',
-            },
-        });
-        return response
-    } 
-    catch (error) {
-        console.error('Error al actualizar la categoría en el servidor');
-    }
-}
-  
+  try {
+    const response = await axios.put(
+      `${WEBURL}fb/categoria/update/${category.id}`,
+      JSON.stringify(category),
+      {
+        headers: {
+          token: token,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error('Error al actualizar la categoría en el servidor', error);
+    throw error;
+  }
+};
